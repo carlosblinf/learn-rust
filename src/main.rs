109ae613c32213c4756b94b3154bd7596cb40b1f
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2023-01-18 15:38:14 -0500

    exercise if/else

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #[derive(PartialEq, Debug)]
-// Declare Car struct to describe vehicle with four named fields
 struct Car {
     color: String,
     motor: Transmission,
     roof: bool,
-    age: (Age,u32), // todo!("Move `mileage: u32` field into `age` field - a tuple with two fields: an `Age` enum, u32");
+    age: (Age, u32),
 }
 
 #[derive(PartialEq, Debug)]
-// Declare enum for Car transmission type
-enum Transmission { Manual, SemiAuto, Automatic }
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
 
 #[derive(PartialEq, Debug)]
-enum Age { New, Used }
+enum Age {
+    New,
+    Used,
+}
 
-fn car_quality (miles: u32) -> (Age, u32) {
-    let mut quality: (Age, u32)= (Age::New, 0);
+fn car_quality(miles: u32) -> (Age, u32) {
+    let mut quality: (Age, u32) = (Age::New, 0);
 
     if miles > 0 {
         quality = (Age::Used, miles);
@@ -24,6 +29,29 @@ fn car_quality (miles: u32) -> (Age, u32) {
     quality
 }
 fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+    if roof {
+        if miles == 0 {
+            println!(
+                "Build a new car: {:?}, {}, Hard top, {} miles",
+                motor, color, miles
+            );
+        } else {
+            println!(
+                "Prepare a used car: {:?}, {}, Hard top, {} miles",
+                motor, color, miles
+            );
+        }
+    } else if miles > 0 {
+        println!(
+            "Prepare a used car: {:?}, {}, Convertible, {} miles",
+            motor, color, miles
+        );
+    } else {
+        println!(
+            "Prepare a used car: {:?}, {}, Hard top, {} miles",
+            motor, color, miles
+        );
+    }
     Car {
         color: color,
         motor: motor,
@@ -33,24 +61,25 @@ fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Ca
 }
 
 fn main() {
-    let car_1 = car_factory(String::from("White"), Transmission::Automatic, true, 100);
-    let car_2 = car_factory(String::from("Yellow"), Transmission::Manual, true, 0);
-    let car_3 = car_factory(String::from("Black"), Transmission::SemiAuto, true, 35);
-    println!(" {:?}\n {:?}\n {:?}", car_1, car_2, car_3);
-
-    let colors = ["Blue", "Green", "Red", "Silver"];
     let mut car: Car;
-    let mut engine = Transmission::Manual;
-
     // Car order #1: New, Manual, Hard top
-    car = car_factory(String::from(colors[0]), engine, true, 0);
-    println!("Car order 1: {:?}, hard top = {}, {:?}, {} {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
-    // Car order #2: New, Semi-automatic, Convertible
-    engine = Transmission::SemiAuto;
-    car = car_factory(String::from(colors[1]), engine, false, 100);
-    println!("Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
-    // Car order #3: New, Automatic, Hard top
-    engine = Transmission::Automatic;
-    car = car_factory(String::from(colors[2]), engine, true, 200);
-    println!("Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+    car = car_factory(String::from("Orange"), Transmission::Manual, true, 0);
+    println!(
+        "Car order 1: {:?}, hard top = {}, {:?}, {} {} miles\n",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #2: Used, Semi-automatic, Convertible
+    car = car_factory(String::from("Red"), Transmission::SemiAuto, false, 565);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles\n",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #3: Used, Automatic, Hard top
+    car = car_factory(String::from("White"), Transmission::Automatic, true, 3000);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles\n",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
