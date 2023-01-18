a09720f21bf08d8e861c3dc6b31c8f2a88193ed4
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2023-01-18 13:00:52 -0500

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 fn main() {
-    // Declare array, initialize all values, compiler infers length = 7
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
-    // Declare array, initialize all values to 0, length = 5
-    let bytes = [0; 5];
+    // Declare vector, initialize with three values
+    let three_nums = vec![15, 3, 46];
+    println!("Initial vector: {:?}", three_nums);
 
-    println!("{:#?} {:#?}", days, bytes);
-}
+    // Declare vector, value = "0", length = 5
+    let zeroes = vec![0; 5];
+    println!("Zeroes: {:?}", zeroes);
+
+    // Create empty vector, declare vector mutable so it can grow and shrink
+    let mut fruit = Vec::new();
+    // Push values onto end of vector, type changes from generic `T` to String
+    fruit.push("Apple");
+    fruit.push("Banana");
+    fruit.push("Cherry");
+    println!("Fruits: {:?}", fruit);
+    // Pop off value at end of vector
+    // Call pop() method from inside println! macro
+    println!("Pop off: {:?}", fruit.pop());
+    println!("Fruits: {:?}", fruit);
+
+    // Declare vector, initialize with three values
+    let mut index_vec = vec![15, 3, 46];
+    let three = index_vec[1];
+    println!("Vector: {:?}, three = {}", index_vec, three);
+    // Add 10 to the value at index 1, which is 3 + 10 = 13
+    index_vec[1] = index_vec[1] + 10;
+    println!("Vector: {:?}", index_vec);
+    }
